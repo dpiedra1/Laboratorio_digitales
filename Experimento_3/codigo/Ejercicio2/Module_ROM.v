33798c611b5a6845d6917328067666eb71b57ac8
dpiedra <danielpiedra25@hotmail.com>
2017-10-04 07:44:42 -0600

    Ejercicio 2 escribe: -HOL M- usando instruccion LCD similar JMP

--- Experimento_3/codigo/Ejercicio2/Module_ROM.v
+++ Experimento_3/codigo/Ejercicio2/Module_ROM.v
@@ -14,13 +14,16 @@ begin
 
 	0: oInstruction = { `NOP ,24'd4000    };
 	1: oInstruction = { `STO , `R7,16'd0  };
-	2: oInstruction = { `STO ,`R3,16'd0     }; 
+	2: oInstruction = { `STO ,`R3,16'd0     };
 	3: oInstruction = { `LCD ,  8'd3,16'b01001000	 };//manda una H
 	4: oInstruction = { `LCD ,  8'd4,16'b01001111	 };//manda una O
 	5: oInstruction = { `LCD ,  8'd5,16'b01001100	 };//manda una L
+	//6: oInstruction = { `LCD ,  8'd6,16'b01000001	 };//manda una A(no funciona)
 	6: oInstruction = { `LCD ,  8'd6,16'b00100000	 };//manda un espacio
 	7: oInstruction = { `LCD ,  8'd7,16'b01001101	 };//manda una M
-	//3: oInstruction = { `LCD ,  8'd3,16'b01000100	 };//manda una D (no sirve)
+	//8: oInstruction = { `LCD ,  8'd8,16'b01010101	 };//manda una U
+	
+	
 	
 	8: oInstruction = { `JMP ,  8'd8,16'b01001000	 };
 	default:
