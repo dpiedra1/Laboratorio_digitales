aebc06c0c725ea8d46696a1db0171ae0009751ae
Radagast1495 <juanjo.navarro1495@gmail.com>
2017-10-25 11:43:09 -0600

    Update Teclado.v

--- Experimento_4/codigo/Ejercicio 2/Teclado.v
+++ Experimento_4/codigo/Ejercicio 2/Teclado.v
@@ -1,1 +1,30 @@
+module Teclado(ClockT, DataT, DataO, Reset);
 
+input wire Clock25;
+input wire DataT;
+output reg [7:0] DataO;
+input wire Reset;
+
+reg [3:0] bitcount;
+reg [7:0] dataForm;
+
+always @(negedge ClockT) begin 
+  if(Reset) begin
+    bitcount<=0;
+  end else if (bitcount>0 && bitcount<9) begin
+      if(bitcount==1) begin
+        dataForm<=DataT;
+        bitcount<=bitcount+4'b1;
+      end else begin
+        dataForm<={dataForm,DataT};
+        bitcount<=bitcount+4'b1;
+      end
+  end else if (bitcount==0) begin
+    bitcount<=bitcount+4'b1;
+  end else begin
+    bitcount<=0;
+    DataO<=dataForm;
+  end
+end
+
+endmodule
